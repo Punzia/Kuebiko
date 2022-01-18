11efb92d952468b987ee04079011c7225da4752a
Punzia <28727157+Punzia@users.noreply.github.com>
2022-01-19 00:38:34 +0100

    Update deploy_commands.js

--- deploy_commands.js
+++ deploy_commands.js
@@ -1,10 +1,17 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-//const { clientId, guildId, token } = require('./config.json');
+const { clientId, guildId, token } = require('./config.json');
 
+
+// The commands are WIP currently.
 const commands = [
-	new SlashCommandBuilder().setName('EnableWorldNewsStream').setDescription('Enable the news feed for Guardian.'),
+	new SlashCommandBuilder().setName('help').setDescription('Show some helpful commands.'),
+	new SlashCommandBuilder().setName('enablejpns').setDescription('Enable the news feed for Japan.'),
+	new SlashCommandBuilder().setName('disablejpns').setDescription('Disable the news feed for Japan.'),
+	new SlashCommandBuilder().setName('enableworldns').setDescription('Enable the news feed for English'),
+	new SlashCommandBuilder().setName('disableworldns').setDescription('Disable the news feed for English'),
+
 ]
 	.map(command => command.toJSON());
 
